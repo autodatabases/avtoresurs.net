f8cd7cc379c54d4e01653cbbb5665d2cff0a0929
x0139 <oleg_a@outlook.com>
2017-02-08 21:03:12 +0200

    added slider

--- main/models.py
+++ main/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Slider(models.Model):
+    image = models.ImageField(verbose_name='Картинка')
+    caption = models.CharField(max_length=100, verbose_name='Заголовок слайда')
+    text = models.CharField(max_length=100, verbose_name='Текст на слайде')
+    added = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name='Добавлена')
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False, verbose_name='Изменена')
+
+    class Meta:
+        ordering = ["-updated"]
+        verbose_name = 'Слайд'
+        verbose_name_plural = 'Слайды'
