ef7da4cbb1bc9c67fc416002b3445a2ae1f5c646
x0139 <oleg_a@outlook.com>
2017-02-08 21:29:47 +0200

    add order to slider

--- main/models.py
+++ main/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+
 
 class Slider(models.Model):
     image = models.ImageField(verbose_name='Картинка')
@@ -8,8 +11,9 @@ class Slider(models.Model):
     text = models.CharField(max_length=100, verbose_name='Текст на слайде')
     added = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name='Добавлена')
     updated = models.DateTimeField(auto_now=True, auto_now_add=False, verbose_name='Изменена')
+    order = models.SmallIntegerField(default=0)
 
     class Meta:
-        ordering = ["-updated"]
+        ordering = ["order"]
         verbose_name = 'Слайд'
         verbose_name_plural = 'Слайды'
