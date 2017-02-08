2c48ec001ebf00b6ffb0725f7a631cf568c910bc
x0139 <oleg_a@outlook.com>
2017-02-08 21:25:41 +0200

    d

--- main/admin.py
+++ main/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from main.models import Slider
 
+
 class SliderAdmin(admin.ModelAdmin):
     list_display = ('caption', 'text', 'updated', 'image.url')
+
 
 admin.site.register(Slider, SliderAdmin)
