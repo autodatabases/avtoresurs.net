56e500402476d03856d7e6eedc5fb97a01163a32
x0139 <oleg_a@outlook.com>
2017-02-08 21:44:53 +0200

    undo last commit

--- main/models.py
+++ main/models.py
@@ -11,7 +11,7 @@ class Slider(models.Model):
     text = models.CharField(max_length=100, verbose_name='Текст на слайде')
     added = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name='Добавлена')
     updated = models.DateTimeField(auto_now=True, auto_now_add=False, verbose_name='Изменена')
-    order = models.SmallIntegerField(default=0)
+    order = models.SmallIntegerField(default=0, verbose_name='Сортировка')
 
     class Meta:
         ordering = ["order"]
