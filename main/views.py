81a70af33c5448dbe4c18eea853c72e782894807
x0139 <oleg_a@outlook.com>
2017-02-20 12:25:40 +0200

    NewsAuto.csv

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ import csv
 
 from django.contrib.sites.shortcuts import get_current_site
 from django.core import signing
+from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template import Context
@@ -14,7 +15,6 @@ from main.models import Slider
 from news.models import Post
 from registration.forms import User
 from shop.models.product import Product
-
 
 # Create your views here.
 # from tecdoc.models import Part
@@ -71,49 +71,44 @@ class ProductLoader(TemplateView):
     def get(self, request):
         get_data = super(ProductLoader, self).get(request)
         get_data = 1
-        path = '/home/avto/avto/NewsAuto.csv'
+        path = 'NewsAuto.csv'
         pass_first_line = True
 
         report = []
 
         with open(path, 'r', encoding='cp1251') as f:
-            if pass_first_line:
-                pass_first_line = False
-                pass
-            reader = csv.reader(f, delimiter=';')
-            for idx, row in enumerate(reader):
-                created = ''
-                try:
-                    # print('sku - %s, brand - %s, title - %s, cross - %s , quantity - %s, active - True, price - %s' %
-                    #       (row[0], row[1], row[2], row[3], row[4], row[5]))
+            data = f.read().splitlines(True)
 
-                    created = Product.objects.get_or_create(
-                        sku=row[0].lower().replace(" ", ""),
-                        manufacturer=row[1].lower(),
-                        title=row[2].lower(),
-                        cross_sku=row[3].lower(),
-                        quantity=row[4],
-                        # quantity=10,
-                        active=True,
-                        price=row[5],
-                        # price=455.12,
+        for idx, line in enumerate(data[1:]):
+            row = line.split(';')
+            created = ''
+            try:
+                created = Product.objects.get_or_create(
+                    sku=row[0].lower().replace(" ", ""),
+                    manufacturer=row[1].lower(),
+                    title=row[2].lower(),
+                    cross_sku=row[3].lower(),
+                    quantity=row[4],
+                    # quantity=10,
+                    active=True,
+                    price=row[5],
+                    # price=455.12,
+                )
+                part = Part.objects.filter(sku__iexact=created.sku, supplier__title__iexact=created.manufacturer)
+                if not part:
+                    error_string = "%s %s %s %s %s" % (
+                        idx,
+                        created.sku,
+                        created.manufacturer,
+                        created.title,
+                        created.cross_sku
                     )
-                    # print(created)
-                    part = Part.objects.filter(sku__iexact=created.sku, supplier__title__iexact=created.manufacturer)
-                    if not part:
-                        error_string = "%s %s %s %s %s" % (
-                            idx,
-                            created.sku,
-                            created.manufacturer,
-                            created.title,
-                            created.cross_sku
-                        )
-                        report.append(error_string)
-                except:
-                    pass
+                    report.append(error_string)
+            except:
+                pass
 
         if report:
-            error_file_path = '/home/avto/avto/error.log'
+            error_file_path = 'error.log'
             report_log = ''
             for error_line in report:
                 error_line += '\n'
@@ -122,7 +117,49 @@ class ProductLoader(TemplateView):
                 # print(report)
                 error_file.write(report_log)
 
-        return get_data
+        return HttpResponse('OK')
+
+        # for idx, row in enumerate(reader):
+        #     created = ''
+        #     try:
+        #         # print('sku - %s, brand - %s, title - %s, cross - %s , quantity - %s, active - True, price - %s' %
+        #         #       (row[0], row[1], row[2], row[3], row[4], row[5]))
+        #
+        #         created = Product.objects.get_or_create(
+        #             sku=row[0].lower().replace(" ", ""),
+        #             manufacturer=row[1].lower(),
+        #             title=row[2].lower(),
+        #             cross_sku=row[3].lower(),
+        #             quantity=row[4],
+        #             # quantity=10,
+        #             active=True,
+        #             price=row[5],
+        #             # price=455.12,
+        #         )
+        #         # print(created)
+        #         part = Part.objects.filter(sku__iexact=created.sku, supplier__title__iexact=created.manufacturer)
+        #         if not part:
+        #             error_string = "%s %s %s %s %s" % (
+        #                 idx,
+        #                 created.sku,
+        #                 created.manufacturer,
+        #                 created.title,
+        #                 created.cross_sku
+        #             )
+        #             report.append(error_string)
+        #     except:
+        #         pass
+        #
+        # if report:
+        #     error_file_path = '/home/avto/avto/error.log'
+        #     report_log = ''
+        #     for error_line in report:
+        #         error_line += '\n'
+        #         report_log += error_line
+        #     with open(error_file_path, 'w+') as error_file:
+        #         # print(report)
+        #         error_file.write(report_log)
+
 
 
 # todo make CBV
