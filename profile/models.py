9e7605ff7c7f65c125b46fe9de766961c55bc15a
x0139 <oleg_a@outlook.com>
2017-03-01 12:23:15 +0200

    added some russian text into profile models

--- profile/models.py
+++ profile/models.py
@@ -6,12 +6,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
     vip_code = models.CharField(max_length=50, blank=True, null=True)
-    fullname = models.CharField(max_length=255, blank=True, null=True)
+    fullname = models.CharField(max_length=255, blank=True, null=True, verbose_name='Полное имя')
     created = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name='Добавлена', blank=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False, verbose_name='Изменена', blank=True)
-    discount = models.ForeignKey('Discount', blank=True, null=True)
+    discount = models.ForeignKey('Discount', blank=True, null=True, verbose_name='Скидка')
 
     def __str__(self):
         return self.user.get_username()
@@ -44,9 +44,9 @@ class Point(models.Model):
 
 
 class Discount(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name='Название скидки')
     discount = models.DecimalField(max_digits=5, decimal_places=2,
-                                   validators=[MinValueValidator(0.0), MaxValueValidator(100)])
+                                   validators=[MinValueValidator(0.0), MaxValueValidator(100)], verbose_name='Размер скидки')
     created = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name='Добавлена')
     updated = models.DateTimeField(auto_now=True, auto_now_add=False, verbose_name='Изменена')
 
