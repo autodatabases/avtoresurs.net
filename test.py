170d0da585e58d91e87a794c77fdeb391a764b6d
x0139 <oleg_a@outlook.com>
2017-03-10 18:00:26 +0200

    small changes

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-a = 1
+import multiprocessing as mp
 
-print([a] * 8)
+def foo(q):
+    q.put('hello')
+
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
+    q = mp.Queue()
+    p = mp.Process(target=foo, args=(q,))
+    p.start()
+    print(q.get())
+    p.join()
