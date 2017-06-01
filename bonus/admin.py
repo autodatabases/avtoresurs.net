d8bcb20e5925735c5aaf0d280ae9e2f08491e8b6
r00tkid <shaman@born2fish.ru>
2017-06-01 19:54:09 +0200

    fix main menu

--- bonus/admin.py
+++ bonus/admin.py
@@ -5,7 +5,8 @@ from bonus.models import Bonus
 
 
 class BonusAdmin(admin.ModelAdmin):
-    list_display = ('id_1c', 'title', 'price', 'created_at','updated_at')
+    list_display = ('id_1c', 'title', 'price', 'created_at', 'updated_at')
+    list_display_links = ('id_1c', 'title', 'price', 'created_at', 'updated_at')
 
 
 admin.site.register(Bonus, BonusAdmin)
