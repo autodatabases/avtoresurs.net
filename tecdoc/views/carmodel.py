ef3dc66572415c83f9fdf15da954a2ef5df2f740
x0139 <oleg_a@outlook.com>
2017-08-03 17:57:52 +0200

    parts tree in progress

--- tecdoc/views/carmodel.py
+++ tecdoc/views/carmodel.py
@@ -2,11 +2,10 @@ from django.views.generic import DetailView
 
 from tecdoc.models import CarType, CarTypeAttributes
 from tecdoc.models.carmodel import CarModel
+from tecdoc.models.tree import CarTree
 
 
 class CarModelView(DetailView):
-    model = CarModel
-    pk_url_kwarg = 'model_id'
 
     def get_context_data(self, **kwargs):
         context = super(CarModelView, self).get_context_data()
@@ -16,7 +15,4 @@ class CarModelView(DetailView):
         for car_type in car_types:
             car_type.car_specs = car_type.car_specs()
             print(car_type.car_specs)
-
-
         return context
-
