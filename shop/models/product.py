3072052e28cf4f2f4ede5eb652f1851c03de7a2c
x0139 <oleg_a@outlook.com>
2017-08-20 15:29:53 +0200

    image fix

--- shop/models/product.py
+++ shop/models/product.py
@@ -134,7 +134,7 @@ class Product(models.Model):
     def image(self):
         tecdoc_image_path = '/static/main/images/tecdoc/'
         part = Part.objects.filter(clean_part_number=self.sku, supplier__title=self.brand).first()
-        image = Image.objects.filter(supplier__title=self.brand, part_number=part.part_number.first())
+        image = Image.objects.filter(supplier__title=self.brand, part_number=part.part_number).first()
         try:
             base, ext = os.path.splitext(image.picture)
             if ext == '.BMP':
