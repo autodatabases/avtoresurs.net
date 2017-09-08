d212c1f41b9ce4b812cd201d997e2f0cc1a71f5f
x0139 <oleg_a@outlook.com>
2017-09-08 11:46:23 +0200

    storages move to command

---
+++ service/management/commands/init_storages.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+
+from shop.models.product import Product, ProductPrice
+from shop.models.storage import Storage, ProductStoragePrice
+
+
+
+class Command(BaseCommand):
+    help = 'initial create ProductStoragePrice'
+
+    def handle(self, *args, **options):
+        product_prices = ProductPrice.objects.all()
+        for product_price in product_prices:
+            product = product_price.product
+            storage = Storage.objects.get(id=1)
+            psp = ProductStoragePrice(product=product, storage=storage, product_price=product)
+            psp.save()
