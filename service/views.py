a34f59e38c8781c11e662caddd792cf5d25d4196
x0139 <oleg_a@outlook.com>
2017-09-14 22:07:26 +0200

    changes in loader

--- service/views.py
+++ service/views.py
@@ -50,7 +50,7 @@ class ProductLoad(TemplateView):
         post_filename = get_filename(self.request.FILES['file'].name)
         filename = default_storage.save(post_filename, ContentFile(file.read()))
         file.close()
-        ProductLoader(filename, storage_id, post_filename)
+        ProductLoader(filename, storage_id, filename)
 
         return HttpResponseRedirect('/service/product_load/')
 
