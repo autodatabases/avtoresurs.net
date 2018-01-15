2443e77d68aa1b3f37e372f178535a8f1f57bdbe
x0139 <oleg_a@outlook.com>
2018-01-15 11:58:53 +0200

    fix bonus

--- bonus/views.py
+++ bonus/views.py
@@ -2,7 +2,7 @@ import json
 import traceback
 
 from django.core.mail import EmailMessage
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from django.views import View
 from django.views.generic import ListView
@@ -59,29 +59,55 @@ class BonusObtainView(View):
         message_admin.save()
 
     def post(self, *args, **kwargs):
-        error_redirect = HttpResponse("Во время получения бонуса произошла ошибка!", status=503)
+        error_redirect = JsonResponse("Неизвестная ошибка! Пожалуйста, обратитесь к администратору сайта.", status=503,
+                                      safe=False)
         try:
-            bonus_id = self.request.GET.get('bonus_id')
-            encoded_data = json.loads(self.request.body.decode('utf8'))
-            user_id = encoded_data['id']
-            key = encoded_data['key']
+            encoded_data = json.loads(self.request.body.decode('utf-8'))
+            bonus_id = encoded_data['bonus']
             bonus = Bonus.objects.get(pk=bonus_id)
-            user = User.objects.get(pk=user_id)
-            if key == PROTECTED_KEY:
-                profile = Profile.objects.get(user=user)
-                if profile.points >= bonus.price:
-                    profile.points -= bonus.price
-                    profile.save()
-                    UserBonus.objects.create(user=user, bonus=bonus)
-                    subject = 'Получение товаров по бонусной акции'
-                    body = 'Пользователь %s получил %s по бонусной акции.' % (user.username, bonus.title)
-                    self._send_email(subject=subject, body=body)
-                    self._send_private_messages_recipient_and_admin(subject=subject, body=body, recipient=user)
-                else:
-                    return error_redirect
+            user = self.request.user
+            profile = Profile.objects.get(user=user)
+            if profile.points >= bonus.price:
+                profile.points -= bonus.price
+                profile.save()
+                UserBonus.objects.create(user=user, bonus=bonus)
+                subject = 'Получение товаров по бонусной акции'
+                body = 'Пользователь %s получил %s по бонусной акции.' % (user.username, bonus.title)
+                self._send_email(subject=subject, body=body)
+                self._send_private_messages_recipient_and_admin(subject=subject, body=body, recipient=user)
             else:
-                return error_redirect
+                miss_points = bonus.price - profile.points
+                return JsonResponse("Недостаточно %s баллов!" % miss_points, status=503, safe=False)
         except Exception as e:
             traceback.print_tb(e)
             return error_redirect
-        return HttpResponse("Вы успешно получили '%s'!" % bonus.title, status=200)
+
+        return JsonResponse("Вы успешно получили '%s'!" % bonus.title, status=200, safe=False)
+
+        # return HttpResponse("Вы успешно получили %s" % bonus_id, status=200)
+        # error_redirect = HttpResponse("Во время получения бонуса произошла ошибка!", status=503)
+        # try:
+        #     bonus_id = self.request.GET.get('bonus_id')
+        #     encoded_data = json.loads(self.request.body.decode('utf8'))
+        #     user_id = encoded_data['id']
+        #     key = encoded_data['key']
+        #     bonus = Bonus.objects.get(pk=bonus_id)
+        #     user = User.objects.get(pk=user_id)
+        #     if key == PROTECTED_KEY:
+        #         profile = Profile.objects.get(user=user)
+        #         if profile.points >= bonus.price:
+        #             profile.points -= bonus.price
+        #             profile.save()
+        #             UserBonus.objects.create(user=user, bonus=bonus)
+        #             subject = 'Получение товаров по бонусной акции'
+        #             body = 'Пользователь %s получил %s по бонусной акции.' % (user.username, bonus.title)
+        #             self._send_email(subject=subject, body=body)
+        #             self._send_private_messages_recipient_and_admin(subject=subject, body=body, recipient=user)
+        #         else:
+        #             return error_redirect
+        #     else:
+        #         return error_redirect
+        # except Exception as e:
+        #     traceback.print_tb(e)
+        #     return error_redirect
+        # return HttpResponse("Вы успешно получили '%s'!" % bonus.title, status=200)
