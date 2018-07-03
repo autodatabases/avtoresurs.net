7a8be13875bb87b6f03ffbaf8454efae8b4cbc24
shaman <shaman@born2fish.ru>
2018-07-03 15:11:24 +0200

    add assortment model to admin

--- assortment/admin.py
+++ assortment/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from assortment.models import AssortmentItem
+
+
+class AssortmentItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated', 'status')
+
+
+admin.site.register(AssortmentItem, AssortmentItemAdmin)
+
