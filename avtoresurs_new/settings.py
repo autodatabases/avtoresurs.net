9baad5800a592fa210e539082791f38b9604eb45
shaman <shaman@born2fish.ru>
2018-07-05 14:46:50 +0200

    stoarage template markup

--- avtoresurs_new/settings.py
+++ avtoresurs_new/settings.py
@@ -170,6 +170,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'main', 'static'),
     os.path.join(BASE_DIR, 'assortment', 'static'),
+    os.path.join(BASE_DIR, 'shop', 'static'),
 )
 
 # "Поисковики" статики. Первый ищет статику в STATICFILES_DIRS,
