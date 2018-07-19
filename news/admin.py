29fc8fe7869e88ddb71ccac67586248d9c3d449a
x0139 <oleg_a@outlook.com>
2018-07-19 21:40:03 +0200

    news admin category added

--- news/admin.py
+++ news/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from news.models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'added', 'updated', 'status')
+    list_display = ('title', 'category', 'added', 'updated', 'status')
 
 admin.site.register(Post, PostAdmin)
 
