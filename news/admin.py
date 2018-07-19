c87752140fa0096a7436ab3a52377b323f38e3b4
x0139 <oleg_a@outlook.com>
2018-07-19 21:49:06 +0200

    editable

--- news/admin.py
+++ news/admin.py
@@ -5,6 +5,8 @@ from news.models import Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'added', 'updated', 'status')
+    list_display_links = ('title', 'added', 'updated', 'status')
+    list_editable = ('category',)
 
 admin.site.register(Post, PostAdmin)
 
