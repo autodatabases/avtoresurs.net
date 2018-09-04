6e57a1b5ae7ca8af87bd806ddf7d3c70524025c4
x0139 <oleg_a@outlook.com>
2018-09-04 09:16:30 +0200

    chages in brands path gen

--- avtoresurs_new/support_utils.py
+++ avtoresurs_new/support_utils.py
@@ -13,6 +13,6 @@ def get_brands_images_list():
     result = []
     for img in brands_images:
         result.append(
-            "{brands_path}{img_name}".format(img_name=img, brands_path=brands_path)
+            os.path.join(os.sep, brands_path, img)
         )
     return result
